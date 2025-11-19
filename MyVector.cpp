86187525f7676f0f40a58414b64cd73597ef1044
Jihwan Jung <21jh@o.cnu.ac.kr>
2025-11-19 16:24:24 +0900

    과제 제출

--- MyVector.cpp
+++ MyVector.cpp
@@ -5,13 +5,56 @@ using namespace std;
 
 template<typename T>
 class MyVector {
-/* TODO */
+private:
+    T* data;            // 동적 배열 포인터
+    size_t capacity;    // 현재 할당된 배열 크기
+    size_t length;      // 실제 저장된 요소 개수
 
+    void resize() {
+        cout << "Resize: " << capacity << " -> " << capacity * 2 << endl;
 
+        size_t newCapacity = capacity * 2;
+        T* newData = new T[newCapacity];
 
+        for (size_t i = 0; i < length; ++i) {
+            newData[i] = data[i];
+        }
 
+        delete[] data;
 
+        data = newData;
+        capacity = newCapacity;
+    }
 
+public:
+    MyVector() : capacity(2), length(0) {
+        data = new T[capacity];
+    }
+
+    ~MyVector() {
+        delete[] data;
+    }
+
+    void push_back(const T& value) {
+        if (length == capacity) {
+            resize();
+        }
+        data[length++] = value;
+    }
+
+    T pop_back() {
+        if (length == 0)
+            throw out_of_range("Vector is empty");
+        return data[--length];
+    }
+
+    T& operator[](const int index) {
+        return data[index];
+    }
+
+    size_t size() const {
+        return length;
+    }
 };
 
 template<typename T>
